842b277c5ee4c20af7ca84a3f16939125669e563
Zachary Hazelwood <zhaze001@gmail.com>
2022-03-23 10:48:31 -0600

    Initial commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,32 @@
 class Venue
+  attr_reader :name, :capacity
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def patrons
+    @patrons
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons(patrons)
+    # upcase_patrons = []
+    # patrons.each do |patron|
+    #   upcase_patrons << patron.upcase
+    @patrons == patron.upcase
+    # end
+    # p upcase_patrons
+  end
+
+  def over_capacity(capacity)
+    if patrons.length > capacity
+      over_capacity == true
+    end
+  end
 end

cb34e222e5aa0f1eaff75c743b063b142056f8b9
Matt Deming <vballmatt44@gmail.com>
2022-03-23 10:46:12 -0600

    Update venue and tests

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,25 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+  def initialize(name, capacity)
+      @name = name
+      @capacity = capacity
+      @patrons = []
+  end
+  def name
+    @name
+  end
+  def capacity
+    @capacity
+  end
+  def patrons
+    @patrons
+  end
+  def add_patron(patron)
+    @patrons << patron
+  end
+  def yell_at_patrons
+    @patrons.each do |patron|
+      patrons.upcase
+    end
+  end
 end

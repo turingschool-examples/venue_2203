d253f4e83bf53c0ae822e1ee1f01f2862a13fab8
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-03-23 12:34:37 -0400

    Pass Test 1 Iteration 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -6,6 +6,14 @@ class Venue
     @capacity = capacity
     @patrons = []
   end
+
+  def add_patron(name)
+    # add_patron.each do |name|
+      @patrons << name
+  end
 end
 
 venue = Venue.new('Bluebird', 4)
+venue.add_patron('Mike')
+venue.add_patron('Megan')
+venue.add_patron('Bob')

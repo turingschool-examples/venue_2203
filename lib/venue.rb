c72090d883254a083a7c28b0fdbad1b762a3298a
Candace Eckels <candace.eckels@gmail.com>
2022-03-23 10:52:34 -0600

    Initial commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,12 @@
 class Venue
+  attr_reader :name, :capacity
+
+  def initialize(name, capacity)
+  @name = name
+  @capacity = capacity
+  @patrons = []
+  end
+
 end
+
+#ven1 = Venue.new("Bluebird", 4, 0)

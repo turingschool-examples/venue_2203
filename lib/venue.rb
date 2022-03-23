71fb259b92bb4e95ad573f3015c44333423f1364
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-03-23 09:24:55 -0600

    Modify iteration 1

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,10 @@
 class Venue
+    attr_reader :name, :venue, :capacity, :patrons
+    def initialize(name, venue, capacity = 4, patrons = [])
+        @name = name
+        @venue = venue
+        @capacity = capacity
+        @patrons = []
+    end
+
 end

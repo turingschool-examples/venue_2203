9d0e58a181f3fb9163f6586e019f5ea0145a1fd0
Eric Espindola <eric_e_2012@hotmail.com>
2022-03-23 09:00:31 -0700

    create Venue class, methods, and tests

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,33 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron)
+    patrons << patron
+  end
+
+  def yell_at_patrons
+    scolded_patrons = []
+    patrons.each do |target|
+      scolded_patrons << target.upcase
+    end
+    return scolded_patrons
+  end
+
+  def over_capacity?
+    return patrons.length > @capacity
+  end
+
+  def kick_out
+    if patrons.length > @capacity
+      patrons.pop
+    end
+  end
 end

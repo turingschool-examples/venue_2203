e17a31242552618836f7c353ff11c7fe6b421ffe
Sage Skaff <skaffsage@gmail.com>
2022-03-23 09:50:35 -0700

    Add venue.rb and venue_spec.rb

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,27 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+
+  end
+
+  def yell_at_patrons
+    patrons.map(&:upcase)
+  end
+
+  def over_capacity?
+    if patrons.count > capacity
+     over_capacity? = true
+   else
+     over_capacity? = false
+  end
 end

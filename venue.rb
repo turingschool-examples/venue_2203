0a7cb1b8f7c3fe462bd5090d2c66041d911a744a
philmarcu <philmark18@gmail.com>
2022-03-23 10:48:44 -0600

    Add more patrons attempt

--- venue.rb
+++ venue.rb
@@ -5,14 +5,21 @@ class Venue
     @capacity = capacity
     @patrons = []
   end
-  def add_patron(name)
-    @patrons << ['Mike', 'Meg', 'Bob']
+  def patrons
+    @patrons = ['Mike', 'Meg', 'Bob']
+  end
+  def add_patrons(patrons)
+    @patrons << patrons
   end
   def yell_at_patrons
-    patrons = ['Mike', 'Meg', 'Bob']
     patrons.each do |patrons|
     p patrons.upcase
   end
+  # def limit
+  #   if @capacity < 5
+  #     @patrons - 1
+  #   end
+  # end
 end
 
 end
@@ -20,5 +27,7 @@ end
 venue = Venue.new('Bluebird', 4, [])
 p venue.name
 p venue.capacity
-p venue.add_patron('name')
+p venue.add_patrons('Dave')
 p venue.yell_at_patrons
+p venue.patrons
+# p venue.limit

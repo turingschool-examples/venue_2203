020c1ab3914ce65cab7e2073be72b35727ba7fbb
Caden Jarrett <caden.jarrett99@gmail.com>
2022-03-23 10:49:31 -0600

    Final commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,21 @@
 class Venue
+  attr_reader :name, :capacity
+  def initialize (name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def patrons
+    @patrons
+end
+  def add_patron(patron)
+    @patrons << patron
+end
+  def yell_at_patrons
+  yell_at_patrons = []
+  patrons.each do |upcase|
+    yell_at_patrons << patrons.upcase
+  end
+end
 end

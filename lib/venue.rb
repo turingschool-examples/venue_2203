e159652a585bde23e216e0b70cd2b86e63a7b797
Sarah Hall <swatson1994@yahoo.com>
2022-03-23 12:04:35 -0400

    Add iterations 1 thru 4, and make pass

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,33 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+  def initialize(name, capacity, patrons = [])
+    @name = name
+    @capacity = capacity
+    @patrons = patrons
+  end
+
+  def add_patron(p_name)
+    patrons << p_name
+  end
+
+  def yell_at_patrons
+    patrons.each do |bad_patron|
+      bad_patron.upcase! #! here returns bad_patrons after changes are made
+      #otherwise we have to reassign patrons or use return
+    end
+  end
+
+  def over_capacity?
+    if patrons.length > capacity
+      true
+    else
+      false
+    end
+  end
+
+  def kick_out
+    until patrons.length <= capacity do
+      patrons.shift
+    end
+  end
 end

a629b4e8598b254b92952400188a106b59550eda
Deannah Burke <deannah@bierstadtlager.com>
2022-03-23 10:04:12 -0600

    Finsih iteration 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,24 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+
+  def initialize (name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron (patron)
+    @patrons = ["Mike", "Megan", "Bob"]
+  end
+
+  def yell_at_patrons
+    patrons = ["Mike", "Megan", "Bob"]
+    capitalized_patrons = []
+    patrons.each do |patron|
+    capitalized_patrons << patron.upcase
+  end
+  p capitalized_patrons
+  end 
+
+
 end

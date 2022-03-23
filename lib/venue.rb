f877fddd59bad2c80f07e0f2e7f3293323b84921
Thiago Silveira <thiago1992@yahoo.com>
2022-03-23 10:36:48 -0600

    Add Readers and Patron Methods

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,5 +1,22 @@
 class Venue
+  attr_reader :name, :capactiy, :patrons
   def initialize(name, capacity)
+      @name = name
+      @capacity = capacity
+      @patrons = []
+  end
 
+  def patrons
+    @patrons
+  end
+
+  def add_patron(patron_name)
+    @patrons << patron_name
+  end
+
+  def yell_at_patrons
+    bad_patrons = []
+    patrons.each do |pat_name|
+      bad_patrons << pat_name.upcase
   end
 end

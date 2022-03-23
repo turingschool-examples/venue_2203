31f1916aad9d060cc4358f09bb53a3931dcedf9b
Clay Ash <claycourtneyash@gmail.com>
2022-03-23 10:27:53 -0600

    Make add_patron pass

--- lib/venue.rb
+++ lib/venue.rb
@@ -5,6 +5,23 @@ class Venue
     @capacity = capacity
     @patrons = []
   end
-end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+ #   def yell_at_patrons
+ #    @patrons.upcase
+ #   end
+ # end
+end 
+
+
 
  venue = Venue.new('Bluebird', 4)
+ venue.add_patron('Mike')
+ venue.add_patron('Megan')
+ venue.add_patron('Bob')
+ venue.patrons
+
+ # venue.yell_at_patrons

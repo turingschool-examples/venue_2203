e38127622ef608b7f8809f7c32fd412cf08f4a78
Stirling Hostetter <stirhoss@icloud.com>
2022-03-23 12:53:42 -0500

    Fix use of instance variables

--- lib/venue.rb
+++ lib/venue.rb
@@ -25,12 +25,12 @@ class Venue
   end
 
   def over_capacity?
-    patrons.count > capacity
+    @patrons.count > @capacity
   end
 
   def kick_out
-    if patrons.count > capacity
-      patrons.pop
+    if @patrons.count > @capacity
+      @patrons.pop
     end
   end
 end

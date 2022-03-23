6bb6d43b101aa1216f1ca804e49325169a78c513
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-03-23 11:37:01 -0400

    initial commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,21 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    yell_patrons = []
+    @patrons.each do |patron|
+    yell_patrons << patron.upcase
+    end
+    p yell_patrons
+  end
 end

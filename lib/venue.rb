a7a3e67ace2668159f624aed1622069bbfbe94dc
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-03-23 09:56:45 -0700

    I'm finished

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,22 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+
+  def initialize(name, capacity)
+    @name  = name
+    @capacity = 4
+    @patrons = []
+    end
+
+    def has_no_patrons
+      @patrons << 0
+    end
+
+    def add_patron(patron)
+    @patrons << patron
+  end
+
+    def yell_at_patrons(patron)
+      @patrons(patron).upcase
+    end
+
 end

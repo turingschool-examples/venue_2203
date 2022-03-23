29483ce1758ce0e0f330592a58c17c8a8a225b39
Jim Riddle <jimriddle0@gmail.com>
2022-03-23 10:36:08 -0500

    Upload files through iteratoin 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,31 @@
 class Venue
+
+ attr_reader :name, :capacity, :patrons
+
+ def initialize(name, capacity)
+   @name = name
+   @capacity = capacity
+   @patrons = []
+ end
+
+ # def patrons
+ #   patrons = []
+ # end
+
+ def add_patron(name)
+   patrons.push(name)
+   return name
+ end
+
+ def yell_at_patrons
+   loud_patrons = []
+   patrons.each do |name|
+     loud_patrons.push(name.upcase)
+   end
+   return loud_patrons
+ end
+
+
+
+
 end

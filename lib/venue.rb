72067a3dc8926e1c61b75f47241a651c1813155b
Paker Thomson <pdthomson1239@gmail.com>
2022-03-23 12:48:07 -0400

    Initial Commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,26 @@
 class Venue
+
+  def initialize(name, capacity)
+@name = name
+@capacity = capacity
+@patrons = []
+def name
+  @name
+end
+
+def capacity
+  @capacity
+end
+
+def patrons
+  @patrons
+end
+
+def add_patron(patrons)
+  @patrons << patrons
+end
+
+def yell_at_patrons
+end
+end
 end
